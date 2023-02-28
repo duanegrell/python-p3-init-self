6acc43d782199ecf541c8b157ad58d9542e71df7
Duane Grell <duanegrell@gmail.com>
2023-02-28 11:41:48 -0500

    passing tests

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,22 @@
 #!/usr/bin/env python3
 
 class Dog:
-    pass
+    def __init__(self, name, breed='Mutt', favorite_toy="Any") :
+        self.name = name
+        # print(f"{name} is born!")
+        self.breed = breed
+        self.favorite_toy = favorite_toy
+
+    def bark(self):
+        print("Woof!")
+
+    def get_adopted(self, owner_name):
+        self.owner = owner_name
+    
+
+fido = Dog("Fido")
+fido.get_adopted("Sophie")
+
+snoopy = Dog("Snoopy", "ball", "Maltese")
+print (snoopy.breed)
+
